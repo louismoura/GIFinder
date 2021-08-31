f6eeae8de81c0d954c53b209f104fa7dbf720edd
Marcos <88422056+marcosmarp@users.noreply.github.com>
2021-08-31 15:27:07 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+function getUserInput() {
+  return document.getElementById("search_text").value;
+}
+
+function pushToDOM(response) {
+  response = JSON.parse(response);
+
+  let images = response.data;
+
+  let container = document.getElementById("gif");
+  container.innerHTML = "";
+
+  images.forEach(function (image) {
+    let src = image.images.fixed_height.url;
+    container.innerHTML += "<img src='" + src + "' class='container-image' />";
+  });
+
+}
+
+function searchGif(key_words) {
+  let api_url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + key_words;
+
+  let api_AJAX_call = new XMLHttpRequest();
+  api_AJAX_call.open("GET", api_url);
+  api_AJAX_call.send();
+
+  api_AJAX_call.addEventListener("load", function (data) {
+    let actual_data = data.target.response;
+    pushToDOM(actual_data);
+    console.log(actual_data);
+  });
+}
+
+document.getElementById("search_button").onclick = function alertUser() {
+  searchGif(getUserInput());
+};
+
+document.getElementById("search_text").addEventListener('keypress', function (key) {
+  if (key.which === 13) { 
+    searchGif(getUserInput());
+  }
+  console.log(key);
+});
