af8b316d211286ea49d342762bb350334af2b99a
Marcos <marcosmartinezpalermo@gmail.com>
2021-09-01 16:16:14 -0300

    Improved mobile experience

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 function getUserInput() {
+  // @ts-ignore
   return document.getElementById("search_text").value;
+}
+
+function hideKeyboard() {
+  setTimeout(function() {
+    var field = document.createElement('input');
+    field.setAttribute('type', 'text');
+    field.setAttribute('style', 'position:absolute; top: 0px; opacity: 0; -webkit-user-modify: read-write-plaintext-only; left:0px;');
+    document.body.appendChild(field);
+    field.onfocus = function(){
+      setTimeout(function() {
+        field.setAttribute('style', 'display:none;');
+        setTimeout(function() {
+          document.body.removeChild(field);
+          document.body.focus();
+        }, 14);
+      }, 200);
+    };
+    field.focus();
+  }, 50);
 }
 
 function pushToDOM(response) {
@@ -9,25 +29,39 @@ function pushToDOM(response) {
   container.innerHTML = "";
   var received_gifs = [];
 
+  console.time("Gifs recovery");
   images.forEach(function (image) {
+    console.log("recovered gif from data");
     let src = image.images.fixed_height.url;
     let gif_temp = "<img src='" + src + "' class='container-image' />";
-    container.innerHTML += gif_temp;
     received_gifs.push(gif_temp)
   });
+  console.log("all gifs recovered");
+  console.timeEnd("Gifs recovery");
+  console.time("Gifs display");
+  for (let i = 0 ; i < received_gifs.length ; ++i) {
+    console.log("sending gif to container");
+    container.innerHTML += received_gifs[i];
+  }
+  console.log("all gifs sent to container");
+  console.timeEnd("Gifs display")
 }
 
 function searchGif(key_words) {
+  console.time("Data request");
+  console.log("started gifs request");
   let api_url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + key_words;
 
   let api_AJAX_call = new XMLHttpRequest();
   api_AJAX_call.open("GET", api_url);
   api_AJAX_call.send();
 
+
   api_AJAX_call.addEventListener("load", function (data) {
-    let actual_data = data.target.response;
-    pushToDOM(actual_data);
-    console.log(actual_data);
+    console.log("received data from request")
+    console.timeEnd("Data request");
+    // @ts-ignore
+    pushToDOM(data.target.response);
   });
 }
 
@@ -41,42 +75,53 @@ function restoreButton() {
   document.getElementById("search_button").style.color = "black";
 }
 
-if (!this.isTouch) {
+let is_touch = !window.matchMedia("(pointer: coarse)").matches;
+
+if (!is_touch) {
+  console.log("mouse-based input");
   document.getElementById("search_button").addEventListener("mouseover", function () {
+    console.log("mouse over search button");
     highlightButton();
   });
   document.getElementById("search_button").addEventListener("mouseout", function() {
+    console.log("mouse left search button");
     restoreButton();
   });
-}
-else if (window.matchMedia("(any-pointer: none)").matches) {
-  document.getElementById("search_button").addEventListener("mousedown", function () {
-    highlightButton();
-  });
 
+  document.getElementById("search_button").addEventListener("mousedown", function () { 
+    console.log("clicking button");
+    });
   document.getElementById("search_button").addEventListener("mouseup", function () {
-    restoreButton();
-  });
+    console.log("button released");
+    searchGif(getUserInput());
+    });
 }
-
-document.getElementById("search_button").addEventListener("click", function() {
-  searchGif(getUserInput());
-});
+else {
+  console.log("touch-based input");
+  document.getElementById("search_button").addEventListener("touchstart", function () { 
+    console.log("touching button");
+    highlightButton();
+    });
+  document.getElementById("search_button").addEventListener("touchend", function () {
+    console.log("button released");
+    restoreButton();
+    hideKeyboard();
+    searchGif(getUserInput());
+    });
+}
 
 document.getElementById("search_text").addEventListener("keydown", function (key) {
-  if (key.which === 13) { 
+  if (key.which === 13) {
+    console.log("enter key pressed in text box"); 
     highlightButton();
   }
 });
 
 document.getElementById("search_text").addEventListener("keyup", function (key) {
   if (key.which === 13) { 
+    console.log("enter key released in text box")
     restoreButton();
-  }
-});
-
-document.getElementById("search_text").addEventListener("keypress", function (key) {
-  if (key.which === 13) { 
+    if (is_touch) hideKeyboard();
     searchGif(getUserInput());
   }
 });
